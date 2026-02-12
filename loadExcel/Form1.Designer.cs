f4dbf8dbbeac53dd33a94d7590c503089565806d
Estudiantes <Estudiantes@S01-301-05>
2026-02-11 20:21:04 -0500

    Agregar archivos de proyecto.

---
+++ loadExcel/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace loadExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadExcel = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadExcel
+            // 
+            this.btnLoadExcel.Location = new System.Drawing.Point(45, 31);
+            this.btnLoadExcel.Name = "btnLoadExcel";
+            this.btnLoadExcel.Size = new System.Drawing.Size(139, 36);
+            this.btnLoadExcel.TabIndex = 0;
+            this.btnLoadExcel.Text = "Cargar Excel...";
+            this.btnLoadExcel.UseVisualStyleBackColor = true;
+            this.btnLoadExcel.Click += new System.EventHandler(this.btnLoadExcel_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(45, 106);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(479, 170);
+            this.dgvDatos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnLoadExcel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadExcel;
+        private System.Windows.Forms.DataGridView dgvDatos;
+    }
+}
+
---
+++ loadExcel/Form1.cs
@@ -0,0 +1,94 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loadExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Empleado { 
+            public string TipoDoc { get; set; }
+            public string NroDoc { get; set; }
+            public decimal Sueldo { get; set; }
+        }
+
+        List<Empleado> empleados = new List<Empleado>();
+        List<string> errores = new List<string>();
+
+        private void btnLoadExcel_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Archivos Excel (*.xlsx)|*.xlsx*";
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    List<Empleado> lista = LeerExcel(ofd.FileName);
+                    dgvDatos.DataSource = lista;
+
+                    dgvDatos.Columns["Sueldo"].DefaultCellStyle.Format = "NO";
+                    dgvDatos.Columns["Sueldo"].DefaultCellStyle.FormatProvider = new System.Globalization.CultureInfo("es-CO");
+                }
+            }
+        }
+
+        private List<Empleado> LeerExcel(string ruta)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(new FileInfo(ruta))) {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int filas = worksheet.Dimension.Rows;
+                for(int fila = 2; fila <= filas; fila++)
+                {
+                    string tipo = worksheet.Cells[fila, 1].Text.Trim().ToUpper();
+                    if(tipo != "CC" && tipo != "CE")
+                    {
+                        MessageBox.Show($"Tipo de documento inválido en fila {fila}. Solo se permite CC o CE.");
+                        continue;
+                    }
+                    empleados.Add(new Empleado
+                    {
+                        TipoDoc = tipo,
+                        NroDoc = worksheet.Cells[fila, 2].Text,
+                        Sueldo = decimal.Parse(worksheet.Cells[fila, 3].Text)
+                    });
+                    decimal sueldo = 0;
+
+                    bool esNumeroValido = decimal.TryParse(
+                        worksheet.Cells[fila, 3].Text,
+                        System.Globalization.NumberStyles.Any,
+                        new System.Globalization.CultureInfo("es-CO"),
+                        out sueldo
+                        );
+
+                    if (!esNumeroValido)
+                    {
+                        errores.Add($"Fila {fila}: Sueldo inválido");
+                        continue;
+                    }
+                    if(sueldo < 0)
+                    {
+                        errores.Add($"Fila {fila}: Sueldo inválido");
+                        continue;
+                    }
+                    if (errores.Any())
+                    {
+                        MessageBox.Show(string.Join("\n", errores));
+                    }
+                }
+            }
+            return empleados;
+        }
+    }
+}
